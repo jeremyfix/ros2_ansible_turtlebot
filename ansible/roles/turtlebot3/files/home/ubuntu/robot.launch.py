151e544fbd3bd9f2e9b461dadeb0a4a7d4ea3b26
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-08-02 09:06:17 +0200

    adds v4l2 node for raspberry CSI camera

--- ansible/roles/turtlebot3/files/home/ubuntu/robot.launch.py
+++ ansible/roles/turtlebot3/files/home/ubuntu/robot.launch.py
@@ -9,7 +9,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
@@ -17,7 +17,7 @@ def generate_launch_description():
     turtlebot3_pkg_dir = LaunchConfiguration(
         "turtlebot3_bringup",
         default=os.path.join(
-            get_package_share_directory("turtlebot3_bringup"), "launch"
+            get_package_share_directory("turtlebot3_bringup"), "launch/"
         ),
     )
 
@@ -27,5 +27,6 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource([turtlebot3_pkg_dir, "robot.launch.py"])
             ),
             # Add the camera node
+            Node(package="v4l2_camera", executable="v4l2_camera_node", name="camera"),
         ]
     )
